edbd0b52316e629b6ab3e6ca74ee19f2816948d9
wolf <3333@qq.com>
2017-03-17 17:28:41 +0800

    sdf

--- java/IP.java
+++ java/IP.java
@@ -14,7 +14,7 @@ class IP {
 //aaaaaaaaaaaaaaaaaaaaaaaaaa
 //bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
 //ccccccccccccccccccccccc
-//sdflksdjglasgdfdsfsdf
+//sdflksdjglasgdfdsfsddfssdfsf
     public static String randomIp() {
         Random r = new Random();
         StringBuffer str = new StringBuffer();
